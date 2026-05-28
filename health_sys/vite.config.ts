36d4b576eefa4f9bbbd328bb246201ea8a2f9c61
the-realdjt <1289505105a@gmail.com>
2026-05-28 11:40:32 +0800

    Update vite.config.ts

--- health_sys/vite.config.ts
+++ health_sys/vite.config.ts
@@ -4,8 +4,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // 🚨 这一行是你的救命代码！专门适配你的 GitHub Pages 地址！
-  base: '/huoshanbei1/health_sys/',
   server: {
     proxy: {
       "/api": "http://127.0.0.1:3000"
